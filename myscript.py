2fdb2f7bdda296a7a33156de41c0c91beab44c9f
SMoragon <smoragon@ucm.es>
2026-04-10 17:55:30 +0200

    Telemetry upgrades

--- myscript.py
+++ myscript.py
@@ -1,5 +1,6 @@
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtWidgets, QtCore
+import pandas as pd
 import random
 import datetime
 import time
@@ -26,9 +27,9 @@ app = QtWidgets.QApplication([])
 patients_data = {
     "Paciente1": "En la entrada",
     "Paciente2": "En la entrada",
-    "Paciente3": "En la entrada"
-   # "Paciente4": "En la entrada",
-   # "Paciente5": "En la entrada"
+    "Paciente3": "En la entrada",
+    "Paciente4": "En la entrada",
+    "Paciente5": "En la entrada"
 }
 
 states_list = [
@@ -51,13 +52,11 @@ patient_last_values = dict()
 plot = pg.PlotWidget(title="Patient data")
 plot.show()
 
-arrarra = 0
-
 init_ms_time = time.time() * 1000
 
 for (i, (name, state)) in enumerate(patients_data.items()):
     
-    bars[i] = list()
+    bars[name] = list()
     init_patient_state_dict(name, state, 0)
     cur_brush = state_to_brush(state)
     patient_last_values[name] = (state, init_ms_time)
@@ -72,15 +71,15 @@ for (i, (name, state)) in enumerate(patients_data.items()):
             pen=pg.mkPen('k', width=2)
         )
 
-    bars[i].append(bar_graph)
+    bars[name].append(bar_graph)
     plot.addItem(bar_graph)
 
 def add_new_patient():
-    new_index = len(patient_to_row)
     
-    bars[new_index] = list()
-    name = f"Paciente {new_index + 1}"
+    new_index = len(patients_data)
+    name = f"Paciente{new_index + 1}"
     
+    bars[name] = list()
     patient_to_row[name] = new_index
     
     state = states_list[0]
@@ -100,7 +99,7 @@ def add_new_patient():
             pen=pg.mkPen('k', width=2)
         )
 
-    bars[new_index].append(bar_graph)
+    bars[name].append(bar_graph)
     plot.addItem(bar_graph)
     
     patients_data[name] = state
@@ -112,6 +111,10 @@ def get_next_state(last_state):
     state_index = states_list.index(last_state)
     return states_list[(state_index+1)%len(states_list)]
 
+def get_previous_state(last_state):
+    state_index = states_list.index(last_state)
+    return states_list[(state_index-1)%len(states_list)]
+
 def modifyValues():
     
     cur_time = time.time() * 1000
@@ -121,40 +124,49 @@ def modifyValues():
         last_state, last_time = patient_last_values[name]
         last_relative_time = get_relative_time(last_time)
         
-        if random.uniform(1, 20) > 19:
-            print(f"Paciente '{name}' se mueve de estado")
+        if state != states_list[-1] and random.uniform(1, 20) > 13:
+            if get_next_state(last_state) == states_list[-1]:
+                print(f"Paciente '{name}' se va a su casa")
             patients_data[name] = get_next_state(last_state)
+        if state != states_list[0] and state != states_list[-1] and random.uniform(1, 20) > 16:
+            patients_data[name] = get_previous_state(last_state)
     
-    if random.uniform(1, 20) > 17:
+    if random.uniform(1, 20) > 15 and len(patients_data)<8:
             add_new_patient()
-            print("newwww")
     
 def update():    
     
     modifyValues()
     
-    axis = plot.getAxis('left')
-    axis.setTicks([[(i, name) for i, name in enumerate(patients_data.keys())]])    
+    active_patients = [act_name for act_name, act_state in patients_data.items()
+                        if act_state != states_list[-1]]
+    
+    for name, state in patients_data.items():
+        if state == states_list[-1]:
+            for bar in bars[name]:
+                plot.removeItem(bar)
+            bars[name] = []
+            
+    patient_to_row.clear()
+    for idx, name in enumerate(active_patients):
+        patient_to_row[name] = idx
+    
+    for name in active_patients:
+        new_y = patient_to_row[name]
+    
+        for bar in bars[name]:
+            bar.setOpts(y=[new_y])
+    
+    axis = plot.getAxis('left')    
+    axis.setTicks([[(i, name) for i, name in enumerate(active_patients)]])
     
     cur_time = time.time() * 1000
     cur_relative_time = get_relative_time(cur_time)
     
-    for (i, (name, state)) in enumerate(patients_data.items()):
+    for name in active_patients:
         
-        if state == states_list[-1]:
-            """for bar_i in bars[i]:
-                last_bar = bar_i  # remove from our list
-                plot.removeItem(last_bar)
-            bars[i][-1].setOpts(
-                    y=0.0001
-                )
-            patients_data.pop(name)
-                
-            last_bar = bars[i].pop()  # remove from our list
-            plot.removeItem(last_bar)  # remove from plot
-            
-            axis.setTicks([[(i, name) for i, name in enumerate(patients_data.keys())]]) """
-            continue
+        state = patients_data[name]
+        bar_idx = patient_to_row[name]
         
         cur_brush = state_to_brush(state)
         
@@ -163,22 +175,22 @@ def update():
             prev_relative_time  = get_relative_time(prev_time)
             
             if prev_state == state:     # No new bar segment needed
-                bars[i][-1].setOpts(
-                    x0 = [bars[i][-1].opts['x0'][0]],
+                bars[name][-1].setOpts(
+                    x0 = [bars[name][-1].opts['x0'][0]],
                     x1 = [cur_relative_time]
                 )
                 patient_state_time[name][state].append(cur_relative_time)
                 
                 
             else:                                   # A new bar segment is required (state transition)
-                last_end = bars[i][-1].opts['x1'][0]
+                last_end = bars[name][-1].opts['x1'][0]
                 
                 new_bar_graph = pg.BarGraphItem(
                     x0 = [last_end],
                     x1 = [cur_relative_time],
                     height=[0.8],
                     brush=cur_brush,
-                    y = [i],
+                    y = [bar_idx],
                     pen=pg.mkPen('k', width=2)
                 )
                 if state not in patient_state_time[name]:
@@ -188,7 +200,7 @@ def update():
                 patient_state_time[name][prev_state].append(cur_relative_time)    # The new state's time begins, and the
                                                                                     # previous one finishes
 
-                bars[i].append(new_bar_graph)
+                bars[name].append(new_bar_graph)
                 plot.addItem(new_bar_graph)
                 patient_last_values[name] = (state, cur_time)
         
@@ -201,11 +213,11 @@ def update():
                     x1 = [cur_relative_time],
                     height=[0.8],
                     brush=cur_brush,
-                    y = [i],
+                    y = [bar_idx],
                     pen=pg.mkPen('k', width=2)
                 )
 
-            bars[i].append(bar_graph)
+            bars[name].append(bar_graph)
             plot.addItem(bar_graph)
         
     plot.setXRange(0, cur_relative_time + 1000)
@@ -216,77 +228,10 @@ plot.showGrid(x=False, y=True)
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(200)  # update every second
+timer.start(1000)  # update every second
 
 app.exec()
 
-
-"""data = {
-    "Barrel1": [0, 1.5, 2, 6],
-    "Barrel2": [0.2, 0.9, 3, 7],
-    "Barrel3": [1, 1.5, 4.2, 8]
-}
-
-brushes = ["b", "r", "y", "w"]
-"""
-
-"""
-for i, (name, barrel_data) in enumerate(data.items()):
-    
-    print(type(barrel_data), barrel_data)
-    
-    bars[i] = list()
-    
-    for j in range(len(barrel_data)-1):
-        
-        start = barrel_data[j]
-        end = barrel_data[j+1]
-        brush = brushes[j%4]
-        
-        print(i, j, start, end, brush)
-        
-        bar_graph = pg.BarGraphItem(
-            x0 = [start],
-            x1 = [end],
-            #width = [end-start-1],
-            height=[0.8],
-            brush=brush,
-            y = [i],
-            pen=pg.mkPen('k', width=2)
-        )
-
-        bars[i].append(bar_graph)
-        plot.addItem(bar_graph)
-
-def update():
-
-    for name, barrel_data in data.items():
-        barrel_data[-1] += pepe#.append(pepe)
-    
-    for ((name, barrel_data), (bar_number, graph_bar)) in zip(data.items(), bars.items()):
-        start = barrel_data[-2]
-        end = barrel_data[-1]
-        graph_bar[-1].setOpts(
-            x0 = [start],
-            x1 = [end]
-        )
-    
-    for (barrel_number, plt_bars) in bars.items():
-        start = data
-        plt_bars[-1].setOpts()
-        
-    for (bar, (name, barrel_data)) in zip(bars, data.items()):
-        for j in range(len(barrel_data)-1):
-        
-            start = barrel_data[j]
-            end = barrel_data[j+1]
-            brush = brushes[j%4]
-            
-            print(i, j, start, end, brush)
-            
-            bar.setOpts(x0 = [start],
-                x1 = [end])
-    
-    pepe += 3
-        
-"""
+df = pd.DataFrame({p: {s: times for s, times in states.items()} for p, states in patient_state_time.items()})
+df.to_csv("pruebas.csv")
+print(df)
